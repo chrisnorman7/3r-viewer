9a5aa41d0da9e615043bff583579d70ec121f5db
Chris Norman <chris.norman2@googlemail.com>
2020-04-24 08:06:37 +0100

    Fixed app title.

--- three_rings_viewer/lib/constants.dart
+++ three_rings_viewer/lib/constants.dart
@@ -1,4 +1,4 @@
-const String appTitle = '3 Rings Viewer';
+const String appTitle = 'Three Rings Viewer';
 const String apiKeyPreferenceName = 'apiKey';
 const String apiKeyFilename = 'api.key';
 const String baseUrl = 'https://www.3r.org.uk';
